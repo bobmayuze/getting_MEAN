010948e269ed60fa297fbfe162747d47de86725a
bobmayuze <584653629@qq.com>
2016-09-03 13:21:30 -0400

    Update
    Listing 4.1 Requiring the controller files in routes/index.js

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+// var ctrlMain = require('../controllers/main');
+// main there refers to main.js
+// Listing 4.1 Requiring the controller files in routes/index.js
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers
+
 
 /* GET home page. */
 router.get('/', ctrlMain.index);
