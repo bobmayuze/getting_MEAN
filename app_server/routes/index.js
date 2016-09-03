b48b14968fa2e74da1249542cecc82c72130198d
bobmayuze <584653629@qq.com>
2016-09-03 13:38:15 -0400

    Update
    Listing 4.2 Defining the routes and mapping them to controllers

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,11 +4,20 @@ var router = express.Router();
 // main there refers to main.js
 // Listing 4.1 Requiring the controller files in routes/index.js
 var ctrlLocations = require('../controllers/locations');
-var ctrlOthers
+var ctrlOthers = require('../controllers/others');
 
 
-/* GET home page. */
+/* GET home pages. */
 router.get('/', ctrlMain.index);
+
+/* GET Location pages. */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationinfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* GET Other pages. */
+router.get('/about', ctrlOthers.about);
+
 
 
 module.exports = router;
