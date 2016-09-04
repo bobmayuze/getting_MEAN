8ae9c75ab66f0b1b8635e5db0ab3186287d7a6af
bobmayuze <584653629@qq.com>
2016-09-04 05:56:52 -0400

    Update
    Update

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,6 @@
 /* GET 'home' page, which is the main or index page in the collections of locations */
 module.exports.homelist = function (req, res) {
-	res.render('index', { title: 'Home' });
+	res.render('locations-list', { title: 'Home' });
 };
 
 /* GET 'Location info' page */
