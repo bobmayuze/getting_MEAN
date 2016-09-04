1c31757b87c9b3d9e44570915f96eff45947bfa8
bobmayuze <584653629@qq.com>
2016-09-04 19:51:06 -0400

    Update
    make to 4.5.4, let’s rush for the chapter 5!

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,31 @@
 /* GET 'home' page, which is the main or index page in the collections of locations */
 module.exports.homelist = function (req, res) {
-	res.render('locations-list', { title: 'Home' });
+	res.render('locations-list', { 
+		title: 'Borilliant',
+		pageHeader: {
+			title: 'Borilliant',
+			strapline: 'Find places to work with WIFI near you!'
+		},
+		locations: [{
+			name: 'Starcups',
+			address: '125 High Street, Reading, RG6 1PS',
+			rating: 3,
+			facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+			distance: '100m'
+		},{
+			name: 'Cafe Hero',
+			address: '123 High Street, Reading, RG6 1PS',
+			rating: 4,
+			facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+			distance: '200m'
+		},{
+			name: 'Burger Queen',
+			address: '121 High Street, Reading, RG6 1PS',
+			rating: 2,
+			facilities: ['Food', 'Premium wifi'],
+			distance: '250m'
+		}]
+	 });
 };
 
 /* GET 'Location info' page */
