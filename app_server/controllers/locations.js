e259770d8b14afd3c2532fa2592b325185728f18
bobmayuze <584653629@qq.com>
2016-09-05 06:13:17 -0400

    Update
    Holy shooot!

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -26,7 +26,7 @@ module.exports.homelist = function (req, res) {
 			facilities: ['Food', 'Premium wifi'],
 			distance: '250m'
 		}]
-	 });
+	});
 };
 
 /* GET 'Location info' page */
@@ -44,33 +44,35 @@ module.exports.locationinfo = function (req, res) {
 		name: 'Starcups',
 		address: '125 High Street, Reading, RG6 1PS',
 		rating: 3,
-		facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+		facilities: [
+			'Hot drinks', 'Food', 'Premium wifi'
+		],
 		distance: '100m',
 		openingTimes: [{
 			days: 'Monday - Friday',
 			opening: '7:00am',
 			closing: '7:00pm',
 			closed: false
-            }, {
+		}, {
 			days: 'Saturday',
 			opening: '8:00am',
 			closing: '5:00pm',
 			closed: false
-            }, {
+		}, {
 			days: 'Sunday',
 			closed: true
-            }],
-            reviews: [{
+		}],
+		reviews: [{
 			author: 'Simon Holmes',
 			rating: 5,
 			timestamp: '16 July 2013',
 			reviewText: 'What a great place. I can\'t say enough good things about it.'
-            }, {
+		}, {
 			author: 'Charlie Chaplin',
 			rating: 3,
 			timestamp: '16 June 2013',
 			reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
-            }]
+		}]
 	}
 	
 
@@ -81,5 +83,11 @@ module.exports.locationinfo = function (req, res) {
 
 /* GET 'Add review' page */
 module.exports.addReview = function (req, res) {
-	res.render('location-review-form', {title: 'Add review'});
+	res.render('location-review-form', {
+		title: 'Add review',
+		sidebar: {
+			context: 'is on Borilliant because it has accessible wifi and space to sit down with your laptop and get some work done.',
+			callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+		}
+	});
 }; 
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,7 @@
 /* GET Other pages. */
 module.exports.about = function (req, res) {
-	res.render('generic-text', {title: 'About the Web'});
-}; 
+	res.render('generic-text', {
+		title: 'About the Web',
+		content: 'Loc8r was created to help people find places to sit down and get a bit of work done.'
+	});
+};  
