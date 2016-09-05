ed60ee4a80e0bbdb3c4046d361e9df3ed28c9117
bobmayuze <584653629@qq.com>
2016-09-05 00:43:48 -0400

    Finish chapter 4
    Yoooooo!

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -36,36 +36,40 @@ module.exports.locationinfo = function (req, res) {
 	pageHeader: {
 		title: 'Starbullshit'
 	},
+	sidebar: {
+		context: 'is on Borilliant because it has accessible wifi and space to sit down with your laptop and get some work done.',
+		callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+	},
 	location: {
-			name: 'Starcups',
-			address: '125 High Street, Reading, RG6 1PS',
-			rating: 3,
-			facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-			distance: '100m',
-            openingTimes: [{
-                days: 'Monday - Friday',
-                opening: '7:00am',
-                closing: '7:00pm',
-                closed: false
+		name: 'Starcups',
+		address: '125 High Street, Reading, RG6 1PS',
+		rating: 3,
+		facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+		distance: '100m',
+		openingTimes: [{
+			days: 'Monday - Friday',
+			opening: '7:00am',
+			closing: '7:00pm',
+			closed: false
             }, {
-                days: 'Saturday',
-                opening: '8:00am',
-                closing: '5:00pm',
-                closed: false
+			days: 'Saturday',
+			opening: '8:00am',
+			closing: '5:00pm',
+			closed: false
             }, {
-                days: 'Sunday',
-                closed: true
+			days: 'Sunday',
+			closed: true
             }],
             reviews: [{
-                author: 'Simon Holmes',
-                rating: 5,
-                timestamp: '16 July 2013',
-                reviewText: 'What a great place. I can\'t say enough good things about it.'
+			author: 'Simon Holmes',
+			rating: 5,
+			timestamp: '16 July 2013',
+			reviewText: 'What a great place. I can\'t say enough good things about it.'
             }, {
-                author: 'Charlie Chaplin',
-                rating: 3,
-                timestamp: '16 June 2013',
-                reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+			author: 'Charlie Chaplin',
+			rating: 3,
+			timestamp: '16 June 2013',
+			reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
             }]
 	}
 	
